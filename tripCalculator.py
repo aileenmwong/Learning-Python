38004e83f58b323636baae1211c4294be25d8270
Aileen Wong <Aileen.Wong@nbcuni.com>
2019-01-03 16:00:27 -0500

    Create tripCalculator.py

---
+++ tripCalculator.py
@@ -0,0 +1,29 @@
+def hotel_cost(nights):
+  return 140*nights
+
+def plane_ride_cost(city):
+  if city == "Charlotte":
+    return 183
+  elif city == "Tampa":
+    return 220
+  elif city == "Pittsburgh":
+    return 222
+  elif city == "Los Angeles":
+    return 475
+
+
+def rental_car_cost(days):
+  total = days * 40
+  if days >= 7:
+  	total -= 50
+  elif days >=3 and days <7:
+  	total -= 20
+  return total
+
+def spending_money(money):
+  return money
+
+def trip_cost(city, days, money):
+  return rental_car_cost(days) + hotel_cost(days - 1) + plane_ride_cost(city) + spending_money(money)
+
+print(trip_cost("Los Angeles", 5, 600))
